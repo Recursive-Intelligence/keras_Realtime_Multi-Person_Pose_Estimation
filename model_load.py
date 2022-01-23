fcceba3f2a8a35605287e5a8a98917ead558e6ab
lakpa-tamang9 <Ld.tamang25@gmail.com>
2022-01-23 13:46:29 +0900

    Fix: Import error for batch notm

--- model_load.py
+++ model_load.py
@@ -4,7 +4,7 @@ from keras.models import Model
 from keras.layers import Input, Dense, Activation, Lambda
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
-from keras.layers.normalization import BatchNormalization
+from keras.layers import BatchNormalization
 from keras.layers.merge import Concatenate
 from config_reader import config_reader
 import scipy
@@ -12,19 +12,23 @@ import math
 
 import json
 
-def relu(x): 
-    return Activation('relu')(x)
+
+def relu(x):
+    return Activation("relu")(x)
+
 
 def conv(x, nf, ks, name):
-    x1 = Conv2D(nf, (ks, ks), padding='same', name=name)(x)
+    x1 = Conv2D(nf, (ks, ks), padding="same", name=name)(x)
     return x1
+
 
 def pooling(x, ks, st, name):
     x = MaxPooling2D((ks, ks), strides=(st, st), name=name)(x)
     return x
 
+
 def vgg_block(x):
-     
+
     # Block 1
     x = conv(x, 64, 3, "conv1_1")
     x = relu(x)
@@ -38,35 +42,36 @@ def vgg_block(x):
     x = conv(x, 128, 3, "conv2_2")
     x = relu(x)
     x = pooling(x, 2, 2, "pool2_1")
-    
+
     # Block 3
     x = conv(x, 256, 3, "conv3_1")
-    x = relu(x)    
+    x = relu(x)
     x = conv(x, 256, 3, "conv3_2")
-    x = relu(x)    
+    x = relu(x)
     x = conv(x, 256, 3, "conv3_3")
-    x = relu(x)    
+    x = relu(x)
     x = conv(x, 256, 3, "conv3_4")
-    x = relu(x)    
+    x = relu(x)
     x = pooling(x, 2, 2, "pool3_1")
-    
+
     # Block 4
     x = conv(x, 512, 3, "conv4_1")
-    x = relu(x)    
+    x = relu(x)
     x = conv(x, 512, 3, "conv4_2")
-    x = relu(x)    
-    
+    x = relu(x)
+
     # Additional non vgg layers
     x = conv(x, 256, 3, "conv4_3_CPM")
     x = relu(x)
     x = conv(x, 128, 3, "conv4_4_CPM")
     x = relu(x)
-    
+
     return x
 
+
 def stage1_block(x, num_p, branch):
-    
-    # Block 1        
+
+    # Block 1
     x = conv(x, 128, 3, "conv5_1_CPM_L%d" % branch)
     x = relu(x)
     x = conv(x, 128, 3, "conv5_2_CPM_L%d" % branch)
@@ -76,12 +81,13 @@ def stage1_block(x, num_p, branch):
     x = conv(x, 512, 1, "conv5_4_CPM_L%d" % branch)
     x = relu(x)
     x = conv(x, num_p, 1, "conv5_5_CPM_L%d" % branch)
-    
+
     return x
 
+
 def stageT_block(x, num_p, stage, branch):
-        
-    # Block 1        
+
+    # Block 1
     x = conv(x, 128, 7, "Mconv1_stage%d_L%d" % (stage, branch))
     x = relu(x)
     x = conv(x, 128, 7, "Mconv2_stage%d_L%d" % (stage, branch))
@@ -95,13 +101,14 @@ def stageT_block(x, num_p, stage, branch):
     x = conv(x, 128, 1, "Mconv6_stage%d_L%d" % (stage, branch))
     x = relu(x)
     x = conv(x, num_p, 1, "Mconv7_stage%d_L%d" % (stage, branch))
-    
+
     return x
 
-weights_path = "keras/model.h5" # orginal weights converted from caffe
-#weights_path = "training/weights.best.h5" # weights tarined from scratch 
 
-input_shape = (None,None,3)
+weights_path = "keras/model.h5"  # orginal weights converted from caffe
+# weights_path = "training/weights.best.h5" # weights tarined from scratch
+
+input_shape = (None, None, 3)
 
 img_input = Input(shape=input_shape)
 
@@ -123,7 +130,7 @@ x = Concatenate()([stage1_branch1_out, stage1_branch2_out, stage0_out])
 for sn in range(2, stages + 1):
     stageT_branch1_out = stageT_block(x, np_branch1, sn, 1)
     stageT_branch2_out = stageT_block(x, np_branch2, sn, 2)
-    if (sn < stages):
+    if sn < stages:
         x = Concatenate()([stageT_branch1_out, stageT_branch2_out, stage0_out])
 
 model = Model(img_input, [stageT_branch1_out, stageT_branch2_out])
